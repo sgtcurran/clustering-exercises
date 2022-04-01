5d101b93781d782aaf4570a2f6b30a58c3f83723
christopher <christopher.curran24@gmail.com>
2022-04-01 16:47:16 -0500

    version 1

--- acquire.py
+++ acquire.py
@@ -192,13 +192,20 @@ def get_zillow_all():
 
     query = '''
     SELECT *
-    FROM `properties_2017`
-    JOIN `predictions_2017` USING (parcelid)
-    JOIN story  
-    WHERE `propertylandusetypeid` = "261" and transactiondate LIKE '2017%%'
+    FROM properties_2017
+    LEFT JOIN airconditioningtype USING (`airconditioningtypeid`)
+    LEFT JOIN architecturalstyletype USING (`architecturalstyletypeid`)
+    LEFT JOIN buildingclasstype USING (`buildingclasstypeid`)
+    LEFT JOIN heatingorsystemtype USING (`heatingorsystemtypeid`)
+    LEFT JOIN propertylandusetype USING (`propertylandusetypeid`)
+    LEFT JOIN storytype USING (`storytypeid`)
+    LEFT JOIN typeconstructiontype USING (`typeconstructiontypeid`)
+    JOIN predictions_2017 ON properties_2017.parcelid = predictions_2017.parcelid 
+    AND predictions_2017.transactiondate LIKE '2017%%'
     '''
     print('Getting a fresh copy from SQL database...')
     df = pd.read_sql(query, url)
     print('Saving to csv...')
     df.to_csv(filename, index=False)
     return df
+#%%
